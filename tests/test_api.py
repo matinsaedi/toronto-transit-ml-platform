2e05801a5b81a730ebf6e62beafc458ff0b9f470
matinsaedi <matinsaedi23@gmail.com>
2026-08-31 18:25:22 -0400

    Add APT tests

---
+++ tests/test_api.py
@@ -0,0 +1,41 @@
+from fastapi.testclient import TestClient
+from toronto_transit_ml_platform.api import app
+
+client = TestClient(app)
+
+def test_health():
+    output = client.get("/health")
+    
+    assert output.status_code == 200
+    assert output.json() == {"status": "healthy"}
+
+def test_predict_valid():
+    response = client.post(
+        "/predict",
+        json={
+              "day": "Wednesday",
+              "line": "102 MARKHAM ROAD",
+              "code": "MFDV",
+              "bound": "N",
+              "month": 8,
+              "hour": 17,
+              },
+        )
+
+    assert response.status_code == 200
+    assert "predicted_delay_minutes" in response.json()
+
+def test_predict_invalid_month():
+    response = client.post(
+        "/predict",
+        json={
+            "day": "Wednesday",
+            "line": "102 MARKHAM ROAD",
+            "code": "MFDV",
+            "bound": "N",
+            "month": 15,
+            "hour": 17,
+            },
+    )
+    
+    assert response.status_code == 422
